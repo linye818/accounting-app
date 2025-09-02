35d16545ab849d9156b395c921ef76f1ace9a6b2
linye818 <71059736+linye818@users.noreply.github.com>
2025-09-02 18:48:40 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/expense_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const AccountingApp());
+  runApp(const MyApp());
 }
 
-class AccountingApp extends StatelessWidget {
-  const AccountingApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Accounting App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Accounting Home'),
-      ),
-      body: const Center(
-        child: Text('Welcome to Accounting App!'),
+    return ChangeNotifierProvider(
+      create: (ctx) => ExpenseProvider(),
+      child: MaterialApp(
+        title: '记账本',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
