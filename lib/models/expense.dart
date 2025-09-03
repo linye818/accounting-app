fc2fd7758bfb10c20810f8f51a8548d02b10deb6
linye818 <71059736+linye818@users.noreply.github.com>
2025-09-03 17:43:45 +0800

    Update expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,38 +1,38 @@
 class Expense {
   final int id;
+  final String description;
   final double amount;
   final int categoryId;
   final DateTime date;
-  final String description;
   final bool isExpense;
 
   Expense({
     required this.id,
+    required this.description,
     required this.amount,
     required this.categoryId,
     required this.date,
-    required this.description,
     required this.isExpense,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
+      'description': description,
       'amount': amount,
       'category_id': categoryId,
       'date': date.millisecondsSinceEpoch,
-      'description': description,
       'is_expense': isExpense ? 1 : 0,
     };
   }
 
-  factory Expense.fromMap(Map<String, dynamic> map) {
+  static Expense fromMap(Map<String, dynamic> map) {
     return Expense(
       id: map['id'],
+      description: map['description'],
       amount: map['amount'],
       categoryId: map['category_id'],
       date: DateTime.fromMillisecondsSinceEpoch(map['date']),
-      description: map['description'],
       isExpense: map['is_expense'] == 1,
     );
   }
