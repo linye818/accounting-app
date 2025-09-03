2a1f6bffb1042ef17cc35988e71d2a7711f51053
linye818 <71059736+linye818@users.noreply.github.com>
2025-09-03 18:14:03 +0800

    Update and rename category.dart to account_category.dart

---
+++ lib/models/account_category.dart
@@ -0,0 +1,39 @@
+class AccountCategory {
+  final int? id;
+  final String name;
+  final String icon;
+  final bool isExpense;
+
+  AccountCategory({
+    this.id,
+    required this.name,
+    required this.icon,
+    required this.isExpense,
+  });
+
+  // 从数据库Map转换为对象
+  static AccountCategory fromMap(Map<String, dynamic> map) {
+    return AccountCategory(
+      id: map['id'],
+      name: map['name'],
+      icon: map['icon'],
+      isExpense: map['is_expense'] == 1,
+    );
+  }
+}
+
+// 默认分类列表
+List<AccountCategory> defaultAccountCategories = [
+  AccountCategory(id: 1, name: '餐饮', icon: 'restaurant', isExpense: true),
+  AccountCategory(id: 2, name: '交通', icon: 'directions_car', isExpense: true),
+  AccountCategory(id: 3, name: '购物', icon: 'shopping_cart', isExpense: true),
+  AccountCategory(id: 4, name: '住房', icon: 'home', isExpense: true),
+  AccountCategory(id: 5, name: '娱乐', icon: 'movie', isExpense: true),
+  AccountCategory(id: 6, name: '医疗', icon: 'local_hospital', isExpense: true),
+  AccountCategory(id: 7, name: '教育', icon: 'school', isExpense: true),
+  AccountCategory(id: 8, name: '其他支出', icon: 'more_horiz', isExpense: true),
+  AccountCategory(id: 101, name: '工资', icon: 'work', isExpense: false),
+  AccountCategory(id: 102, name: '奖金', icon: 'card_giftcard', isExpense: false),
+  AccountCategory(id: 103, name: '投资收益', icon: 'trending_up', isExpense: false),
+  AccountCategory(id: 104, name: '其他收入', icon: 'more_horiz', isExpense: false),
+];
